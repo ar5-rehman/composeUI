07c0c5f29e5f312e8ae094eea2fe72ab18b2b6f2
ar5-rehman <ark1111.khan@gmail.com>
2022-09-19 11:07:43 +0500

    text compose

--- app/src/main/java/com/test/composeui/MainActivity.kt
+++ app/src/main/java/com/test/composeui/MainActivity.kt
@@ -1,15 +1,20 @@
 package com.test.composeui
 
+import android.R
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.test.composeui.ui.theme.ComposeUITheme
 
 class MainActivity : ComponentActivity() {
@@ -22,6 +27,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
+                    //ScaffoldSample()
                     Greeting("Android")
                 }
             }
@@ -31,13 +37,36 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    Text(text = "Hello $name!",
+         Modifier.size(12.dp),
+         fontFamily = FontFamily.SansSerif,
+         color = colorResource(R.color.black)
+    )
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     ComposeUITheme {
+        //ScaffoldSample()
         Greeting("Android")
     }
+}
+
+@Composable
+fun ScaffoldSample() {
+    val scaffoldState = rememberScaffoldState(rememberDrawerState(DrawerValue.Closed))
+    Scaffold(
+        scaffoldState = scaffoldState,
+        topBar = { TopAppBar(title = {Text("Top App Bar")},backgroundColor = colorResource(id = R.color.black), contentColor = colorResource(
+            id = R.color.white
+        ))  },
+        floatingActionButtonPosition = FabPosition.End,
+        floatingActionButton = { FloatingActionButton(onClick = {}){
+            Icon(imageVector = Icons.Default.Add, contentDescription = "fab icon")
+        } },
+        drawerContent = { Text(text = "Drawer Menu 1") },
+        content = { Text("Content") },
+        bottomBar = { BottomAppBar(backgroundColor = MaterialTheme.colors.primary) { Text("Bottom App Bar") } }
+    )
 }
